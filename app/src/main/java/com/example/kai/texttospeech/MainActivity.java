53b68605a4ff8dd8caa8798d94311c2ab50dd9cf
KaiOrland <kayorland@gmail.com>
2017-11-30 23:45:37 +0200

    hch

--- app/src/main/java/com/example/kai/texttospeech/MainActivity.java
+++ app/src/main/java/com/example/kai/texttospeech/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.kai.texttospeech;
 
 import android.app.SearchManager;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
@@ -24,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     Button bt2;
     String input;
     String output;
-
+    String test;
     private final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView txtSpeechInput;
     @Override
@@ -32,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ed1 = (EditText)findViewById(R.id.editText);
-        bt1 = (Button)findViewById(R.id.speak);
-        bt2 = (Button)findViewById(R.id.record);
+       // bt1 = (Button)findViewById(R.id.speak);
+        bt2 = (Button)findViewById(R.id.talk);
         txtSpeechInput = (TextView) findViewById(R.id.textView);
         t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -42,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                     t1.setLanguage(Locale.US);
             }
         });
-        bt1.setOnClickListener(new View.OnClickListener(){
+     /*  bt1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 if(ed1.getText().toString().equals("")!=true) {
@@ -51,29 +55,29 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else {
                     input = txtSpeechInput.getText().toString();
-                    /*if (input.indexOf("search") != -1) {
-                        output = "searching" + input.substring(6);
-                        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                        intent.putExtra(SearchManager.QUERY, input.substring(6));
-                        startActivity(intent);
-
-                    } else
-                        output = input;*/
                     output = AI(input);
                 }
 
                 Toast.makeText(getApplicationContext(), output, Toast.LENGTH_SHORT).show();
                 t1.speak(output, TextToSpeech.QUEUE_FLUSH, null);
-
-
-
-
             }
-        });
+        });*/
         bt2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 promptSpeechInput();
+
+                if(ed1.getText().toString().equals("")!=true) {
+                    input = ed1.getText().toString();
+                    output = AI(input);
+                }
+                else {
+                    input = txtSpeechInput.getText().toString();
+                    output = AI(input);
+                }
+
+                Toast.makeText(getApplicationContext(), output, Toast.LENGTH_SHORT).show();
+                t1.speak(output, TextToSpeech.QUEUE_FLUSH, null);
             }
         });
     }
@@ -114,16 +118,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String AI(String in){
-        String out;
+        String out = in;
         if (in.indexOf("search") != -1) {
             out = "searching" + in.substring(6);
             Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
             intent.putExtra(SearchManager.QUERY, in.substring(6));
             startActivity(intent);
+        }
 
-        } else
-            out = in;
+        if(in.indexOf("call") != -1){
+            out = "openning phone";
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            boolean hasDigit = in.matches(".*\\d+.*");
+            if(hasDigit) {
+                String phoneNumber = getPhoneNumber(in);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+            }
+            startActivity(intent);
+        }
+        if(in.indexOf("music") != -1){
+            out = "openning music";
+            openApp(this, "com.google.android.music");
+        }
+        if(in.indexOf("weather") != -1){
+            out = "searching for weather";
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, "weather");
+            startActivity(intent);
+        }
+
         return out;
     }
 
+    public static boolean openApp(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        Intent i = manager.getLaunchIntentForPackage(packageName);
+        if (i == null) {
+            return false;
+
+        }
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        context.startActivity(i);
+        return true;
+    }
+
+    public String getPhoneNumber(String str){
+        String phoneNumber = "";
+        for(int i =0;i<str.length();i++){
+            if(Character.isDigit(str.charAt(i)))
+                phoneNumber = phoneNumber + str.charAt(i);
+        }
+        return phoneNumber;
+    }
 }
