53241c2b1f34e977a11538baf76d065e4bfa4ffe
KaiOrland <kayorland@gmail.com>
2018-01-30 13:12:31 +0200

    sdh

---
+++ app/src/main/java/com/example/kai/texttospeech/Preferences.java
@@ -0,0 +1,18 @@
+package com.example.kai.texttospeech;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Madaim on 30/01/2018.
+ */
+
+public class Preferences extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preferences);
+    }
+}
